550a50d9360cbc312f87221b1a9a2a793b5cc441
Kaori Tsushima <kaoritsushima0107@gmail.com>
2023-08-19 23:54:01 +0100

    added seasoning

--- src/pages/GettingStarted.jsx
+++ src/pages/GettingStarted.jsx
@@ -1,4 +1,24 @@
 import React from "react";
+import Dashi from "../resources/dashi.jpg";
+import Miso from "../resources/miso.jpg";
+import Ponzu from "../resources/ponzu.jpg";
+
+function Seasoning({ title, img, substitute }) {
+  return (
+    <div className="col-lg-2">
+      <h4>{title}</h4>
+      <img src={img} alt={title} height={200} />
+      {substitute ? (
+        <>
+          <p>
+            <strong>Substitute</strong>
+          </p>
+          <p>{substitute}</p>
+        </>
+      ) : null}
+    </div>
+  );
+}
 
 export default function GettingStarted() {
   return (
@@ -8,7 +28,7 @@ export default function GettingStarted() {
       </h2>
       <h3>Common seasonings</h3>
       <div className="row mt-5 mb-5">
-        <div className="col-lg-4">
+        <div className="col-lg-2">
           <h4>Soy sause</h4>
           <img
             src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/093/973/original/soy-sause.jpg?1692452819"
@@ -16,7 +36,7 @@ export default function GettingStarted() {
             height={200}
           />
         </div>
-        <div className="col-lg-4">
+        <div className="col-lg-2">
           <h4>Mirin</h4>
           <img
             src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/093/976/original/mirin-2.jpg?1692453198"
@@ -28,7 +48,7 @@ export default function GettingStarted() {
           </p>
           <p>white wine : sugar = 3:1</p>
         </div>
-        <div className="col-lg-4">
+        <div className="col-lg-2">
           <h4>Cooking Sake</h4>
           <img
             src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/093/975/original/cooking-sake.jpg?1692453087"
@@ -40,7 +60,20 @@ export default function GettingStarted() {
           </p>
           <p>water</p>
         </div>
+        <div className="col-lg-2">
+          <h4>Miso</h4>
+          <img src={Miso} alt="miso" height={200} />
+        </div>
+        <div className="col-lg-2">
+          <h4>Ponzu</h4>
+          <img src={Ponzu} alt="ponzu" height={200} />
+        </div>
+        <div className="col-lg-2">
+          <h4>Dashi Stock</h4>
+          <img src={Dashi} alt="dashi stock" height={200} />
+        </div>
       </div>
+
       <h3>Common mix sauses</h3>
       <div className="row mt-5">
         <div className="col-lg-4">
