42139b1b7c77d745045c7f1f240efb43817e77a0
Kaori Tsushima <kaoritsushima0107@gmail.com>
2023-08-19 10:06:49 +0100

    added concept

---
+++ src/Concept.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Concept() {
+  return (
+    <div className="Concept">
+      <p>
+        Japanese people love to enjoy ingridient taste itself (e.g. sashimi).
+      </p>
+      <p>So, I believe "Simple is the best!"</p>
+      <p>
+        My recipes are very simple, so let's cooking end enjoying Japanese food!
+      </p>
+    </div>
+  );
+}
